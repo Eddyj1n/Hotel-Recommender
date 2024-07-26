ff2af28f09a21f76caad8cabfddd902a183e4d8c
Eddy <edwardjin.96@gmail.com>
2024-07-26 12:09:59 +1000

    deployment

--- recommender.R
+++ recommender.R
@@ -1,16 +1,27 @@
+# Install and load required packages --------------------------------------
+required_packages <- c("dplyr", "tidyr", "recommenderlab", "here")
+
+# Define Function to install missing packages
+install_if_missing <- function(p) {
+  if (!require(p, character.only = TRUE)) {
+    install.packages(p, dependencies = TRUE)
+    library(p, character.only = TRUE)
+  }
+}
+
+# Install missing packages
+invisible(sapply(required_packages, install_if_missing))
 
 # Load packages -----------------------------------------------------------
 library(dplyr)
 library(tidyr)
 library(recommenderlab)
-
-# Clear work space 
-rm(list=ls())
+library(here)
 
 # Read and Preprocess Data ------------------------------------------------
 
 # Read in required data sets [Change code to automatically read in]
-activity_df <- read.delim("~/xero_assessment/data/activity.txt")
+activity_df <- read.delim(here("data", "activity.txt"))
 
 # Engineer Features to Parse into Models -----------------------------------
   
@@ -29,14 +40,8 @@ user_item_matrix_real  <- as(user_item_matrix, "binaryRatingMatrix")
 # Train ICBF model 
 ibcf_model <- Recommender(user_item_matrix_real, method = "IBCF", parameter = list(method = "Cosine", k=30))
 
-# Train Popular model 
-popular_model <- Recommender(user_item_matrix_real, method = "POPULAR")
-
 # Predict the top N recommendations for UBCF model 
 ibcf_predictions <- predict(ibcf_model,user_item_matrix_real, type = "topNList", n = 1)
-
-# Predict the top N recommendations for POPULAR model
-popular_predictions <- predict(popular_model,user_item_matrix_real, type = "topNList", n = 1)
 
 # Retrieve Top Recommendations --------------------------------------------
 
@@ -47,26 +52,29 @@ ibcf_top_1_pred <- bestN(ibcf_predictions, n = 1)
 # Extract the top 1 recommendation for each user
 ibcf_top_1_list <- getList(ibcf_top_1_pred)
 
-# Convert the list to a dataframe
-# Filter out users with no recommendations
+# Convert the list to a data  frame
 ibcf_top_1_df <- do.call(rbind, lapply(names(ibcf_top_1_list), function(user) {
+  # Put control statement if edge cases exists where user has not recommendation 
   if(length(ibcf_top_1_list[[user]]) > 0) {
     data.frame(User = user, Hotel = ibcf_top_1_list[[user]])
   }
 }))
 
-# POPULAR model 
-# Get the top 1 recommendation for each user
-popular_top_1_pred <- bestN(popular_predictions, n = 1)
+# Output Predictions ------------------------------------------------------
 
-# Extract the top 1 recommendation for each user
-popular_top_1_list <- getList(popular_top_1_pred)
+# Ensure the predictions directory exists
+if (!dir.exists(here("predictions"))) {
+  dir.create(here("predictions"))
+}
 
-# Convert the list to a dataframe
-popular_top_1_df <- do.call(rbind, lapply(names(popular_top_1_list), function(user) {
-  data.frame(User = user, Hotel = popular_top_1_list[[user]])
-}))
+# Export top recommendations to a tab delimited file named "Top_hotel_recommendation.txt" 
+output_path <- here("predictions", "Top_hotel_recommendation.txt")
+write.table(ibcf_top_1_df, file = output_path, 
+            sep = "\t", 
+            row.names = FALSE, 
+            col.names = TRUE, 
+            quote = FALSE)
 
-
-
+# Print completion message
+cat("PREDICTION COMPLETE - Output located in", output_path, "\n")
   
